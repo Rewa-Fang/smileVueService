9012c4ca696895b2cf10804ba6019fae01cd4324
Rewa Fang <fangrh0627@163.com>
2018-07-24 00:35:23 +0800

    login & registry

--- appApi/User.js
+++ appApi/User.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router');
 let userRouter = new Router();
+const mongoose = require('mongoose');
 
 userRouter.get('/',async(ctx)=>{
   ctx.body = 'this is user / ';
@@ -8,8 +9,52 @@ userRouter.get('/',async(ctx)=>{
 userRouter.post('/register',async(ctx)=>{
   let postData = ctx.request.body;
   console.log(postData+'----test----');
-  ctx.body= postData;
+  let User = mongoose.model('User')
+  let newUser = new User(postData);
+
+  await newUser.save().then(()=>{
+    ctx.body = {
+      code:200,
+      message:'register success'
+    }
+  }).catch(error=>{
+    ctx.body = {
+      code:500,
+      message:error
+    }
+  })
+
 });
 
+userRouter.post('/login',async(ctx)=>{
+  let userLogin = ctx.request.body;
+  let userName = userLogin.userName;
+  let password = userLogin.password;
+  const User = mongoose.model('User');
+  await User.findOne({userName:userName}).exec().then(async(result)=>{
+    if(result){
+      let newUser = new User();
+      await newUser.comparePassword(password,result.password)
+      .then((isMatch)=>{
+        ctx.body = {code:200,message:isMatch}
+      })
+      .catch(error=>{
+        console.log(error);
+        ctx.body = {
+          code:500,message:error
+        }
+      })
+    }else{
+      ctx.body = {
+        code:500,message:'userName is not existent'
+      }
+    }
+  }).catch(error=>{
+    console.log(error);
+    ctx.body = {
+      code:500,message:error
+    }
+  })
+})
 
 module.exports = userRouter;
--- index.js
+++ index.js
@@ -16,20 +16,20 @@ _router.use('/user',userRouter.routes())
 app.use(_router.routes())
 app.use(_router.allowedMethods())
 //立即执行
-// ;(async()=>{
-//   await connect(); //连接数据库
-//   initSchemas(); // 引入schema模型
-//   const User = mongoose.model('User'); // 定义User模型 对应数据库中users 集合
-//   let oneUser = new User({userName:'rewaFang',password:'456789'}); //定义一个User模型实例
-//   oneUser.save().then(()=>{ //保存 插入数据库
-//     console.log('insert success');
-//   });
-//   let users = await User.find({}).exec();  // 查询所有User模型数据
-//   console.log('---------------');
-//   console.log(users);
-//   console.log('---------------');
+;(async()=>{
+  await connect(); //连接数据库
+  initSchemas(); // 引入schema模型
+  // const User = mongoose.model('User'); // 定义User模型 对应数据库中users 集合
+  // let oneUser = new User({userName:'rewaFang',password:'456789'}); //定义一个User模型实例
+  // oneUser.save().then(()=>{ //保存 插入数据库
+  //   console.log('insert success');
+  // });
+  // let users = await User.find({}).exec();  // 查询所有User模型数据
+  // console.log('---------------');
+  // console.log(users);
+  // console.log('---------------');
   
-// })();
+})();
 
 // app.use(async(ctx)=>{
 //   ctx.body = '<h1>Hello world!  smileVueService</h1>'
