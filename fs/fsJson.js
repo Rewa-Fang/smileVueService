2e8f630bdc74174c3ff2fb04254e97e704cf7216
Rewa Fang <fangrh0627@163.com>
2018-07-29 00:44:12 +0800

    fs readfile error,json Unexpected token u in JSON at position 0

---
+++ fs/fsJson.js
@@ -0,0 +1,35 @@
+// const fs = require('fs');
+
+// fs.readFile('.goods.json','utf8',function(err,data){
+//     let newData = JSON.parse(data);
+//     let i = 0;
+//     let pushData = [];
+//     newData.RECORDS.map((value,index)=>{
+//         if(value.IMAGE1){
+//             i++;
+//             console.log(value.NAME);
+//             pushData.push(value);
+//         }
+//     })
+//     console.log(i);
+//     console.log(pushData);
+// })
+
+const fs = require('fs')
+
+
+fs.readFile('.goods.json', 'utf8', function(err, data){
+
+    let newData= JSON.parse(data)
+    let i=0
+    let pushData=[]
+    newData.RECORDS.map(function(value,index){
+        if(value.IMAGE1!=null){
+            i++
+            console.log(value.NAME)
+            pushData.push(value)
+        }  
+    })
+    console.log(i)
+    console.log(pushData)
+});
