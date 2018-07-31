92e18144d2c0ef40d58200c48da82139617f2f4a
Rewa Fang <fangrh0627@163.com>
2018-07-31 18:28:28 +0800

    fs read goods json,fs write newGoods json

--- fs/fsJson.js
+++ fs/fsJson.js
@@ -1,35 +1,24 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// fs.readFile('.goods.json','utf8',function(err,data){
-//     let newData = JSON.parse(data);
-//     let i = 0;
-//     let pushData = [];
-//     newData.RECORDS.map((value,index)=>{
-//         if(value.IMAGE1){
-//             i++;
-//             console.log(value.NAME);
-//             pushData.push(value);
-//         }
-//     })
-//     console.log(i);
-//     console.log(pushData);
-// })
-
-const fs = require('fs')
-
-
-fs.readFile('.goods.json', 'utf8', function(err, data){
-
-    let newData= JSON.parse(data)
-    let i=0
-    let pushData=[]
-    newData.RECORDS.map(function(value,index){
-        if(value.IMAGE1!=null){
-            i++
-            console.log(value.NAME)
-            pushData.push(value)
-        }  
+fs.readFile('goods.json','utf8',function(err,data){
+    let newData = JSON.parse(data);
+    let i = 0;
+    let pushData = [];
+    newData.RECORDS.map((value,index)=>{
+        if(value.IMAGE1){
+            i++;
+            console.log(value.NAME);
+            pushData.push(value);
+        }
     })
-    console.log(i)
-    console.log(pushData)
-});
+    console.log(i);
+    // console.log(pushData);
+
+    fs.writeFile('./newGoods.json',JSON.stringify(pushData),(err)=>{
+        if(err){
+            console.log('fail');
+        }else{
+            console.log('success');
+        }
+    });
+})
